dab97f0165f1999dc2e91eff40ddfa414d2d36cf
Srihari Saiyapu <saiyapusrihari2006@gmail.com>
2026-08-06 14:06:04 +0530

    Time: 4 ms (100%), Space: 46 MB (53.76%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int rem=0,rev=0;
+        int temp=x;
+        while(x>0)
+        {
+            rem=x%10;
+            rev=rev*10+rem;
+            x/=10;
+        }
+        if(rev==temp)
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+}
